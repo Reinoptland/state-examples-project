15516220e8951976366ee8ad50af48e813e19a96
Reinoptland <reinoptland@gmail.com>
2021-05-25 16:40:14 +0200

    Event handler externaliseren

--- src/PlayerScore.js
+++ src/PlayerScore.js
@@ -1,9 +1,17 @@
 function PlayerScore({ avatar, name, score, setScore, setEditmode, editMode }) {
+  function increment() {
+    if (name === "Rein") {
+      setScore(score + 10);
+    } else {
+      setScore(score + 1);
+    }
+  }
+
   return (
     <div>
       {avatar} - {name}: {score}
       {score === 10 && <p>Jij bent de winnaar</p>}
-      <button onClick={() => setScore(score + 1)}>+</button>
+      <button onClick={increment}>+</button>
       <button onClick={() => setScore(score - 1)}>-</button>
       <button onClick={() => setEditmode(!editMode)}>✍️</button>
     </div>
