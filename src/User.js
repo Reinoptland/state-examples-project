ec9a1178aac864765d17f91947b15f1b7b21adce
Reinoptland <reinoptland@gmail.com>
2021-05-25 15:41:13 +0200

    Render edit form conditionally

--- src/User.js
+++ src/User.js
@@ -8,32 +8,39 @@ function User() {
   const [score, setScore] = useState(0);
   const [name, setName] = useState("Rein");
   const [avatar, setAvatar] = useState("🤪");
+  const [editMode, setEditmode] = useState(false);
+
+  // formulier laten verdwijnen
+  // boolean, true of false -> conditional render
+  // onClick
+  // submit -> false
+  // pennetje -> true
 
   return (
     <div>
-      <div>
-        <label>playername</label>
-        <input value={name} onChange={(event) => setName(event.target.value)} />
-        <button>submit</button>
-        <select
-          value={avatar}
-          onChange={(event) => setAvatar(event.target.value)}
-        >
-          <option>🤪</option>
-          <option>💪</option>
-          <option>🥵</option>
-        </select>
-      </div>
+      {editMode && (
+        <div>
+          <label>playername</label>
+          <input
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <button onClick={() => setEditmode(false)}>submit</button>
+          <select
+            value={avatar}
+            onChange={(event) => setAvatar(event.target.value)}
+          >
+            <option>🤪</option>
+            <option>💪</option>
+            <option>🥵</option>
+          </select>
+        </div>
+      )}
       {avatar} - {name}: {score}
       {score === 10 && <p>Jij bent de winnaar</p>}
-      {/* {score === 10 ? (
-        <p>Jij bent de winnaar</p>
-      ) : (
-        <p>Jij moet beter je best doen</p>
-      )} */}
       <button onClick={() => setScore(score + 1)}>+</button>
       <button onClick={() => setScore(score - 1)}>-</button>
-      <button>✍️</button>
+      <button onClick={() => setEditmode(!editMode)}>✍️</button>
     </div>
   );
 }
